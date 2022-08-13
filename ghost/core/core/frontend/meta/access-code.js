92c3580943b34d2abacf5bc77e992c1b9904a269
Paul Baumgart <paul@baumgart.us>
2022-08-13 16:04:57 -0700

    Only add a Public Access Link if the site is private

--- ghost/core/core/frontend/meta/access-code.js
+++ ghost/core/core/frontend/meta/access-code.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 function getAccessCode(data, settingsCache) {
     const context = data.context ? data.context : null;
 
-    if (settingsCache && settingsCache.get('password') && _.includes(context, 'post')) {
+    if (settingsCache && settingsCache.get('is_private') && _.includes(context, 'post')) {
         let hasher = crypto.createHash('sha256');
         hasher.update(getUrl(data, false).replace(/\//g, '') + settingsCache.get('password'), 'utf8');
         return hasher.digest('hex');
